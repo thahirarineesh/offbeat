258a43572a347f83472291f990462146f3da99b3
thahirarineesh <114098131+thahirarineesh@users.noreply.github.com>
2024-04-11 19:09:48 +0530

    Update urls.py

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('admin_logout/',views.admin_logout,name='admin_logout'),
     path('order/',views.order,name = 'order'),
+    path('admin_order_details/<int:order_id>/', views.admin_order_details, name='admin_order_details'),
     path('update_order/', views.updateorder, name='update_order'),
     path('cancel_order/', views.cancel_order, name='cancel_order'),
     path('return_order/', views.return_order, name='user_return_order'),
